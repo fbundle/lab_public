dfee9f7271cded61c1450e17db140a3b75fa0569
khanh <khanh.nguyen.contact@gmail.com>
2025-04-18 01:31:57 +0800

    auto_push

--- pkg/uint1792/dft.go
+++ pkg/uint1792/dft.go
@@ -7,7 +7,11 @@ import (
 
 type DFT func(x Block, n int, omega uint64) (y Block)
 
-var dft DFT = CooleyTukeyFFT
+var dft DFT = func(x Block, n int, omega uint64) (y Block) {
+	out := CooleyTukeyFFT(x[:], omega)
+	copy(y[:], out)
+	return y
+}
 
 func SetDefaultDFT(f DFT) {
 	dft = f
@@ -41,8 +45,8 @@ func NaiveDFT(x []uint64, omega uint64) (y []uint64) {
 	return y
 }
 
-// CooleyTukeyFFT :Cooley-Tukey algorithm
-func CooleyTukeyFFT(x Block, n int, omega uint64) (y Block) {
+// CooleyTukeyBlockFFT :Cooley-Tukey algorithm
+func CooleyTukeyBlockFFT(x Block, n int, omega uint64) (y Block) {
 	if n == 1 {
 		return x
 	}
@@ -55,8 +59,8 @@ func CooleyTukeyFFT(x Block, n int, omega uint64) (y Block) {
 		o[i] = x[2*i+1]
 	}
 	nextOmega := mul(omega, omega)
-	eFFT := CooleyTukeyFFT(e, n/2, nextOmega)
-	oFFT := CooleyTukeyFFT(o, n/2, nextOmega)
+	eFFT := CooleyTukeyBlockFFT(e, n/2, nextOmega)
+	oFFT := CooleyTukeyBlockFFT(o, n/2, nextOmega)
 
 	var omegaPow uint64 = 1 // omega^0
 	for i := 0; i < n/2; i++ {
@@ -68,48 +72,31 @@ func CooleyTukeyFFT(x Block, n int, omega uint64) (y Block) {
 	return y
 }
 
-func MixedRadixFFT192(x Block, omega uint64) (y Block) {
-	// Factor n = r * s
-	r, s := 3, 64
-
-	// Reshape x into matrix x[r][s]
-	matrix := make([]Block, r)
-	for i := range matrix {
-		for j := 0; j < s; j++ {
-			matrix[i][j] = x[i*s+j]
-		}
+// CooleyTukeyFFT :Cooley-Tukey algorithm
+func CooleyTukeyFFT(x []uint64, omega uint64) (y []uint64) {
+	n := len(x)
+	if n == 1 {
+		return x
 	}
-
-	// Row-wise FFTs (length s)
-	for i := 0; i < r; i++ {
-		matrix[i] = CooleyTukeyFFT(matrix[i], s, pow(omega, uint64(r))) // ω^r
+	if n <= 0 || n%2 != 0 {
+		panic("n must be power of 2")
 	}
-
-	// Twiddle factors
-	for i := 0; i < r; i++ {
-		for j := 0; j < s; j++ {
-			w := pow(omega, uint64(i*j)) // ω^{i*j}
-			matrix[i][j] = mul(matrix[i][j], w)
-		}
+	var e, o []uint64 // even and odd values of x
+	for i := 0; i < n/2; i++ {
+		e = append(e, x[2*i])
+		o = append(o, x[2*i+1])
 	}
+	nextOmega := mul(omega, omega)
+	eFFT := CooleyTukeyFFT(e, nextOmega)
+	oFFT := CooleyTukeyFFT(o, nextOmega)
 
-	// Column-wise FFTs (length r)
-	for j := 0; j < s; j++ {
-		col := make([]uint64, r)
-		for i := 0; i < r; i++ {
-			col[i] = matrix[i][j]
-		}
-		col = NaiveDFT(col, pow(omega, uint64(s))) // ω^s
-		for i := 0; i < r; i++ {
-			matrix[i][j] = col[i]
-		}
-	}
-
-	// Flatten output
-	for i := 0; i < r; i++ {
-		for j := 0; j < s; j++ {
-			y[i*s+j] = matrix[i][j]
-		}
+	y = make([]uint64, n)
+	var omegaPow uint64 = 1 // omega^0
+	for i := 0; i < n/2; i++ {
+		t := mul(omegaPow, oFFT[i])
+		y[i] = add(eFFT[i], t)
+		y[i+n/2] = sub(eFFT[i], t)
+		omegaPow = mul(omegaPow, omega)
 	}
 	return y
 }
