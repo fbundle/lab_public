67e82184f84af379c44ff284db6c5be050e22af9
khanh <khanh.nguyen.contact@gmail.com>
2025-04-28 22:27:40 +0800

    auto_push

---
+++ cmd/fib/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"ca/pkg/fib"
+	"ca/pkg/integer"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("Usage: go run main.go <integer>")
+		return
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("Error: argument must be an integer")
+		return
+	}
+
+	_ = fib.Fib(integer.Int{}, uint64(n))
+}
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"ca/pkg/fib"
+	"ca/pkg/integer"
+	"ca/pkg/padic"
+	"ca/pkg/ring"
+	"ca/pkg/uint_ntt"
 	"fmt"
-	"sync/atomic"
+	"os"
+	"strconv"
 )
 
+func testPAdic() {
+	const N = 10
+	x := padic.NewPAdicFromInt(3, 23)
+	y := padic.NewPAdicFromInt(3, 27)
+	z := padic.NewPAdicFromInt(3, 92)
+	fmt.Println(x.Approx(N))
+	fmt.Println(y.Approx(N))
+	fmt.Println(y.Mul(x).Approx(N)) // print 27 x 23 = 621
+	fmt.Println(y.Sub(x).Approx(N)) // print 27 - 23 = 4
+	fmt.Println(z.Div(x).Approx(N)) // print 92 / 23 = 4
+}
+
+func testFib() {
+	if len(os.Args) < 2 {
+		panic("Usage: go run main.go <integer>")
+		return
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("Error: argument must be an integer")
+		return
+	}
+
+	fmt.Println(fib.Fib(uint_ntt.UintNTT{}, uint64(n)))
+}
+
+func testEA() {
+	a, b := ring.EuclideanAlgorithm(integer.FromInt64(15), integer.FromInt64(46))
+	fmt.Println(a, b)
+}
+
+func testUintNTT() {
+	x := uint_ntt.FromString("0x318346193417412890342342")
+	z := uint_ntt.FromString("0x539543980a084524")
+	fmt.Println(z)
+	fmt.Println(z.Add(x).Mod(x))
+}
+
 func main() {
-	var a atomic.Value
-	swapped := a.CompareAndSwap(nil, 1)
-	fmt.Println(swapped, a.Load())
+	testUintNTT()
 }
