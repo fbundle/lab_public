27759c5f8da362f48b37a352a38ed9819579fc6e
khanh <khanh.nguyen.contact@gmail.com>
2025-06-11 02:37:16 +0800

    auto_push

--- pkg/monad/math.go
+++ pkg/monad/math.go
@@ -11,6 +11,13 @@ var Fibonacci Monad[int] = func() Iterator[int] {
 var Prime Monad[int] = func() Iterator[int] {
 	i := Natural.DropAtMost(2)
 	return func() (int, bool) {
-
+		p, ok := i.Head()
+		if !ok {
+			panic("not ok")
+		}
+		i = Filter(i.DropAtMost(1), func(n int) bool {
+			return n%p != 0 // keep those n not divided by p
+		})
+		return p, true
 	}
 }
