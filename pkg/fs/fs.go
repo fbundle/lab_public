92563d29ae6c2317ec5eae7eadec4e9796797cb8
khanh <khanh.nguyen.contact@gmail.com>
2025-08-20 21:29:29 +0800

    auto_push

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -1,10 +1,5 @@
 package fs
 
-import (
-	"errors"
-	"sync"
-)
-
 type Closer = func() error
 
 type File interface {
@@ -23,68 +18,14 @@ type FileWriter interface {
 	Truncate(length uint64) error
 }
 
-func NewMemFile() File {
-	return &memFile{
-		mu: sync.RWMutex{},
-		file: memOpenedFile{
-			data: nil,
-		},
-	}
-}
+type Name = string
 
-type memFile struct {
-	mu   sync.RWMutex
-	file memOpenedFile
-}
+type Path = []Name
 
-func (f *memFile) OpenRead() (FileReader, Closer, error) {
-	f.mu.RLock()
-	return &f.file, func() error {
-		f.mu.RUnlock()
-		return nil
-	}, nil
-}
+type FileSystem interface {
+	OpenOrCreate(path Path) (File, error)
+	Delete(path Path) error
 
-func (f *memFile) OpenWrite() (FileWriter, Closer, error) {
-	f.mu.Lock()
-	return &f.file, func() error {
-		f.mu.Unlock()
-		return nil
-	}, nil
-}
-
-type memOpenedFile struct {
-	data []byte
-}
-
-func (f *memOpenedFile) Size() uint64 {
-	return uint64(len(f.data))
-}
-
-func (f *memOpenedFile) Read(offset uint64, length uint64, reader func([]byte)) error {
-	if offset > uint64(len(f.data)) {
-		return errors.New("index out of range")
-	}
-	remaining := uint64(len(f.data)) - offset
-	if length > remaining {
-		length = remaining
-	}
-	reader(f.data[offset : offset+length])
-	return nil
-}
-func (f *memOpenedFile) Write(offset uint64, length uint64, writer func([]byte)) error {
-	if offset+length > uint64(len(f.data)) {
-		f.data = append(f.data, make([]byte, offset+length-uint64(len(f.data)))...)
-	}
-	writer(f.data[offset : offset+length])
-	return nil
-}
-func (f *memOpenedFile) Truncate(length uint64) error {
-	if length > uint64(len(f.data)) {
-		f.data = append(f.data, make([]byte, length-uint64(len(f.data)))...)
-	}
-	if length < uint64(len(f.data)) {
-		f.data = f.data[0:length]
-	}
-	return nil
+	List(prefix Path) func(yield func(name Name, file File) bool)
+	Walk() func(yield func(path Path, file File) bool)
 }
