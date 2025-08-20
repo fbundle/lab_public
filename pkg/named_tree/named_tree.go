f874c855ab142237a36fe954413caf54b8de028d
khanh <khanh.nguyen.contact@gmail.com>
2025-08-20 14:27:52 +0800

    auto_push

--- pkg/named_tree/named_tree.go
+++ pkg/named_tree/named_tree.go
@@ -2,46 +2,46 @@ package named_tree
 
 type Tree[T any] struct {
 	Data     T
-	ChildMap map[string]*Tree[T]
+	children map[string]*Tree[T]
 }
 
 func (n *Tree[T]) Set(name string, child *Tree[T]) *Tree[T] {
-	if n.ChildMap == nil {
-		n.ChildMap = make(map[string]*Tree[T])
+	if n.children == nil {
+		n.children = make(map[string]*Tree[T])
 	}
-	if _, ok := n.ChildMap[name]; ok {
+	if _, ok := n.children[name]; ok {
 		panic("child name already exists")
 	}
-	n.ChildMap[name] = child
+	n.children[name] = child
 	return child
 }
 
 func (n *Tree[T]) Get(name string) (*Tree[T], bool) {
-	if n.ChildMap == nil {
+	if n.children == nil {
 		return nil, false
 	}
-	child, ok := n.ChildMap[name]
+	child, ok := n.children[name]
 	return child, ok
 }
 
 func (n *Tree[T]) Del(name string) {
-	if n.ChildMap == nil {
+	if n.children == nil {
 		panic("child name does not exist")
 	}
-	if _, ok := n.ChildMap[name]; !ok {
+	if _, ok := n.children[name]; !ok {
 		panic("child name does not exist")
 	}
-	delete(n.ChildMap, name)
-	if len(n.ChildMap) == 0 {
-		n.ChildMap = nil
+	delete(n.children, name)
+	if len(n.children) == 0 {
+		n.children = nil
 	}
 }
 
 func (n *Tree[T]) Iter(yield func(name string, child *Tree[T]) bool) {
-	if n.ChildMap == nil {
+	if n.children == nil {
 		return
 	}
-	for name, child := range n.ChildMap {
+	for name, child := range n.children {
 		if ok := yield(name, child); !ok {
 			return
 		}
@@ -52,7 +52,7 @@ func (n *Tree[T]) Resolve(path []string) *Tree[T] {
 	if len(path) == 0 {
 		return n
 	}
-	if n.ChildMap == nil {
+	if n.children == nil {
 		return nil
 	}
 	name, path := path[0], path[1:]
@@ -68,10 +68,10 @@ func (n *Tree[T]) Walk(prefix []string) func(yield func(path []string, node *Tre
 		if ok := yield(prefix, n); !ok {
 			return
 		}
-		if n.ChildMap == nil {
+		if n.children == nil {
 			return
 		}
-		for name, child := range n.ChildMap {
+		for name, child := range n.children {
 			for path, node := range child.Walk(append(prefix, name)) {
 				if ok := yield(path, node); !ok {
 					return
