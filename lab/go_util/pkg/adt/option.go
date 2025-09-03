f3dc0bdbc61b05b2dc03cab211000f026c509bef
khanh <khanh.nguyen.contact@gmail.com>
2025-09-03 20:59:22 +0800

    auto_push

--- lab/go_util/pkg/adt/option.go
+++ lab/go_util/pkg/adt/option.go
@@ -30,6 +30,26 @@ func Some[T any](val T) Option[T] {
 	}
 }
 
+type Option2[T1 any, T2 any] struct {
+	Val1 T1
+	Val2 T2
+	Ok   bool
+}
+
+func None2[T any]() Option[T] {
+	return Option[T]{
+		Ok: false,
+	}
+}
+
+func Some2[T1 any, T2 any](val1 T1, val2 T2) Option2[T1, T2] {
+	return Option2[T1, T2]{
+		Val1: val1,
+		Val2: val2,
+		Ok:   true,
+	}
+}
+
 func Cast[T any](v any) Option[T] {
 	val, ok := v.(T)
 	return Option[T]{
