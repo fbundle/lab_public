cb120ea72e2d1e7f7e30671f539788f10dac1d46
khanh <khanh.nguyen.contact@gmail.com>
2025-09-04 17:53:29 +0800

    auto_push

--- lab/go_util/pkg/adt/slice.go
+++ lab/go_util/pkg/adt/slice.go
@@ -1,14 +1,11 @@
 package adt
 
-type Slice[T any] []T
-
-func (s Slice[T]) NonEmpty() Option[NonEmptySlice[T]] {
+func NonEmpty[T any](s []T) Option[NonEmptySlice[T]] {
 	if len(s) == 0 {
 		return None[NonEmptySlice[T]]()
 	} else {
-		return Some(NonEmptySlice[T](s))
+		return Some[NonEmptySlice[T]](s)
 	}
-
 }
 
 type NonEmptySlice[T any] []T
@@ -17,14 +14,14 @@ func (s NonEmptySlice[T]) Head() T {
 	return s[0]
 }
 
-func (s NonEmptySlice[T]) Tail() Slice[T] {
-	return Slice[T](s[1:])
+func (s NonEmptySlice[T]) Tail() []T {
+	return s[1:]
 }
 
 func (s NonEmptySlice[T]) Last() T {
 	return s[len(s)-1]
 }
 
-func (s NonEmptySlice[T]) Init() Slice[T] {
-	return Slice[T](s[:len(s)-1])
+func (s NonEmptySlice[T]) Init() []T {
+	return s[:len(s)-1]
 }
