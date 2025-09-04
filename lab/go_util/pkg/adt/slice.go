c95ab956a35551508be1983f336c9539839086f6
khanh <khanh.nguyen.contact@gmail.com>
2025-09-04 18:05:17 +0800

    auto_push

--- lab/go_util/pkg/adt/slice.go
+++ lab/go_util/pkg/adt/slice.go
@@ -4,24 +4,36 @@ func NonEmpty[T any](s []T) Option[NonEmptySlice[T]] {
 	if len(s) == 0 {
 		return None[NonEmptySlice[T]]()
 	} else {
-		return Some[NonEmptySlice[T]](s)
+		return Some[NonEmptySlice[T]](nonEmptySlice[T](s))
 	}
 }
 
-type NonEmptySlice[T any] []T
+type NonEmptySlice[T any] interface {
+	Repr() []T
+	Head() T
+	Tail() []T
+	Last() T
+	Init() []T
+}
 
-func (s NonEmptySlice[T]) Head() T {
+type nonEmptySlice[T any] []T
+
+func (s nonEmptySlice[T]) Repr() []T {
+	return s
+}
+
+func (s nonEmptySlice[T]) Head() T {
 	return s[0]
 }
 
-func (s NonEmptySlice[T]) Tail() []T {
+func (s nonEmptySlice[T]) Tail() []T {
 	return s[1:]
 }
 
-func (s NonEmptySlice[T]) Last() T {
+func (s nonEmptySlice[T]) Last() T {
 	return s[len(s)-1]
 }
 
-func (s NonEmptySlice[T]) Init() []T {
+func (s nonEmptySlice[T]) Init() []T {
 	return s[:len(s)-1]
 }
